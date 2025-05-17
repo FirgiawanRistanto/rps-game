34351a781867fadfab358e192237a19783b93dc3
FirgiawanRistanto <firgiawanristanto1@gmail.com>
2025-05-17 18:46:55 +0700

    initial commit

--- lib/gestures/paper.ts
+++
@@ -1,6 +0,0 @@
-import * as fp from 'fingerpose';
-const paper = new fp.GestureDescription('paper');
-for (let finger of fp.Finger.all) {
-  paper.addCurl(finger, fp.FingerCurl.NoCurl, 1.0);
-}
-export default paper;   
--- lib/gestures/rock.ts
+++
@@ -1,7 +0,0 @@
-import * as fp from 'fingerpose';
-const rock = new fp.GestureDescription('rock');
-for (let finger of [fp.Finger.Index, fp.Finger.Middle, fp.Finger.Ring, fp.Finger.Pinky]) {
-  rock.addCurl(finger, fp.FingerCurl.FullCurl, 1.0);
-}
-rock.addCurl(fp.Finger.Thumb, fp.FingerCurl.HalfCurl, 0.5);
-export default rock;
